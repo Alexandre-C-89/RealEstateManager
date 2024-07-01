a5f2f22e2030bc733749fe1261fcf46aa000b9fa
AlexQobuz <alexandre.clemencot@qobuz.com>
2024-07-01 16:43:15 +0200

    Create Shapes for ui

---
+++ app/src/main/java/com/example/realestatemanager/designsystem/ui/Shape.kt
@@ -0,0 +1,41 @@
+package com.example.realestatemanager.designsystem.ui
+
+import androidx.compose.ui.unit.dp
+
+object Shapes
+
+// Default
+inline val Shapes.Default
+    get() = 16.dp
+
+// Large
+inline val Shapes.Large
+    get() = 24.dp
+
+// ExtraLarge
+inline val Shapes.ExtraLarge
+    get() = 32.dp
+
+// GigaLarge
+inline val Shapes.GigaLarge
+    get() = 48.dp
+
+// Mega
+inline val Shapes.Mega
+    get() = 64.dp
+
+// Medium
+inline val Shapes.Medium
+    get() = 12.dp
+
+// Small
+inline val Shapes.Small
+    get() = 8.dp
+
+// Tiny
+inline val Shapes.Tiny
+    get() = 4.dp
+
+// Nano
+inline val Shapes.Nano
+    get() = 2.dp
