63dd43745d8e4c955722980369ac5cf5c50fd8cf
AlexQobuz <alexandre.clemencot@qobuz.com>
2024-07-01 16:46:52 +0200

    Create Title object

---
+++ app/src/main/java/com/example/realestatemanager/designsystem/ui/text/Title.kt
@@ -0,0 +1,78 @@
+package com.example.realestatemanager.designsystem.ui.text
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+object Text {
+    @Composable
+    fun Big(text: String) = Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = text,
+        style = TextStyle(
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = fonts,
+            textAlign = TextAlign.Start,
+            color = Theme.colorPalette.textPrimary
+        )
+    )
+
+    @Composable
+    fun Large(text: String) = Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = text,
+        style = TextStyle(
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Medium,
+            fontFamily = fonts,
+            textAlign = TextAlign.Start,
+            color = Theme.colorPalette.textPrimary
+        )
+    )
+
+    @Composable
+    fun Medium(text: String) = Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = text,
+        style = TextStyle(
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+            fontFamily = fonts,
+            textAlign = TextAlign.Start,
+            color = Theme.colorPalette.textPrimary
+        )
+    )
+
+    @Composable
+    fun Default(text: String) = Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = text,
+        style = TextStyle(
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            fontFamily = fonts,
+            textAlign = TextAlign.Start,
+            color = Theme.colorPalette.textPrimary
+        )
+    )
+
+    @Composable
+    fun Small(text: String) = Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = text,
+        style = TextStyle(
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+            fontFamily = fonts,
+            textAlign = TextAlign.Start,
+            color = MediumGrey
+        )
+    )
+
+}
