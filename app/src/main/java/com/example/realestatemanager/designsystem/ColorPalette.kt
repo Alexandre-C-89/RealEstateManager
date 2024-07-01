59f58379183f2d18e440884fac8529bfb6fe863c
AlexQobuz <alexandre.clemencot@qobuz.com>
2024-07-01 17:01:17 +0200

    Create Color

---
+++ app/src/main/java/com/example/realestatemanager/designsystem/ColorPalette.kt
@@ -0,0 +1,44 @@
+package com.example.realestatemanager.designsystem
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import java.io.File.separator
+
+data class ColorPalette(
+    val spinner: Color,
+    val separator: Color,
+    val textPrimary: Color,
+    val textSecondary: Color,
+    val textDisabled: Color,
+    val surfacePrimary: Color,
+    val surfaceSecondary: Color,
+    val iconPrimary: Color,
+    val iconSecondary: Color,
+    val iconDisabled: Color
+)
+
+val darkColorPalette = ColorPalette(
+    spinner = White,
+    separator = Grey,
+    textPrimary = White,
+    textSecondary = Grey,
+    textDisabled = LightGrey,
+    surfacePrimary = White,
+    surfaceSecondary = DarkGrey,
+    iconPrimary = White,
+    iconSecondary = LightGrey,
+    iconDisabled = LightGrey,
+)
+
+val lightColorPalette = ColorPalette(
+    spinner = Blue,
+    separator = Grey,
+    textPrimary = DarkGrey,
+    textSecondary = LightGrey,
+    textDisabled = LightGrey,
+    surfacePrimary = White,
+    surfaceSecondary = LightGrey,
+    iconPrimary = DarkGrey,
+    iconSecondary = Grey,
+    iconDisabled = LightGrey,
+)
---
+++ app/src/main/java/com/example/realestatemanager/designsystem/Colors.kt
@@ -0,0 +1,14 @@
+package com.example.realestatemanager.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
+val DarkBlue = Color(0xFF094A75)
+val Blue = Color(0xFF127BC0)
+val LightBlue = Color(0xFF49ADEE)
+
+val Grey = Color(0xFF323232)
+val DarkGrey = Color(0xFF323232)
+val LightGrey = Color(0xFF999898)
