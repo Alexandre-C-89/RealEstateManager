139a3f1305eda1eab266b063cdf16790e084d71a
AlexQobuz <alexandre.clemencot@qobuz.com>
2024-10-03 17:00:25 +0200

    Create test for conversion Euro to Dollar

--- app/src/androidTest/java/com/example/realestatemanager/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/realestatemanager/ExampleInstrumentedTest.kt
@@ -15,10 +15,37 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.realestatemanager", appContext.packageName)
     }
+
+    val EURO_TO_DOLLAR_RATE = 1.1
+    val DOLLAR_TO_EURO_RATE = 1 / EURO_TO_DOLLAR_RATE
+
+    fun convertEuroToDollar(euro: Double): Double {
+        return euro * EURO_TO_DOLLAR_RATE
+    }
+
+    fun convertDollarToEuro(dollar: Double): Double {
+        return dollar * DOLLAR_TO_EURO_RATE
+    }
+
+    @Test
+    fun testConvertEuroToDollar() {
+        val euros = 100.0
+        val expectedDollars = euros * EURO_TO_DOLLAR_RATE
+        assertEquals(expectedDollars, convertEuroToDollar(euros), 0.001)
+    }
+
+    @Test
+    fun testConvertDollarToEuro() {
+        val dollars = 100.0
+        val expectedEuros = dollars * DOLLAR_TO_EURO_RATE
+        assertEquals(expectedEuros, convertDollarToEuro(dollars), 0.001)
+    }
+
 }
