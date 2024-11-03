8bf5f2e106b6da03e1940cd1622a5b3a3d23fc2c
AlexQobuz <alexandre.clemencot@qobuz.com>
2024-11-03 15:24:47 +0100

    Update test content provider

--- app/src/test/java/com/example/realestatemanager/ContentProviderTest.kt
+++ app/src/test/java/com/example/realestatemanager/ContentProviderTest.kt
@@ -4,6 +4,8 @@ import android.database.Cursor
 import android.net.Uri
 import com.example.realestatemanager.data.local.contentProvider.ContentProvider
 import com.example.realestatemanager.data.local.property.PropertyDao
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertSame
 import org.junit.Before
@@ -38,6 +40,10 @@ class ContentProviderTest {
 
         // Simulate behavior from DAO for retrieve the mockedCursor
         Mockito.`when`(propertyDao.getAllPropertiesAsCursor()).thenReturn(mockedCursor)
+        Mockito.`when`(mockedCursor.moveToFirst()).thenReturn(true)
+        Mockito.`when`(mockedCursor.moveToNext()).thenReturn(true, false)
+        Mockito.`when`(mockedCursor.getString(Mockito.eq(0))).thenReturn("Property 1")
+        Mockito.`when`(mockedCursor.getInt(Mockito.eq(1))).thenReturn(100000)
 
         // Call query method from ContentProvider
         val uri = mock(Uri::class.java)
@@ -47,5 +53,9 @@ class ContentProviderTest {
         verify(propertyDao).getAllPropertiesAsCursor()
         assertNotNull(cursor) // Check if Cursor isn't null
         assertSame(mockedCursor, cursor) // Check if the Cursor is same
+        assertNotNull(cursor)
+        assertTrue(cursor!!.moveToFirst())
+        assertEquals("Property 1", cursor.getString(0))
+        assertEquals(100000, cursor.getInt(1))
     }
 }
